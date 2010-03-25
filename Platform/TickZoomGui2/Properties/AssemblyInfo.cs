3a4799d02565aa5d052deea8c8374bdc48dd9d41
AutoBuild <wayne@tickzoom.com>
2010-03-25 00:10:07 -0400

    Build version 0.8.1.142.

--- Platform/TickZoomGui2/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui2/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.8.4.1281")]
+[assembly: AssemblyVersion("0.8.1.142")]
 //[assembly: AssemblyFileVersion("2.0.0.0")]
--- Providers/eSignal/eSTests/Properties/AssemblyInfo.cs
+++ Providers/eSignal/eSTests/Properties/AssemblyInfo.cs
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("0.7.4.1389")]
+[assembly: AssemblyVersion("0.8.1.142")]
