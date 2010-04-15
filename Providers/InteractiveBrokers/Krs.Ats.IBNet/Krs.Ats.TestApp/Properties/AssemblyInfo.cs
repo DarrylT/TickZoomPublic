30aca69c1294629a1f621102ecafb1d28693d95f
AutoBuild <wayne@tickzoom.com>
2010-04-15 15:20:09 -0400

    Build version 0.8.4.232.

--- Providers/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
+++ Providers/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.8.4.97")]
+[assembly: AssemblyVersion("0.8.4.232")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
